e93f710f917d24637135c0148b5c7f0be890e04e
Matthew Wynn <matthew@matthewwynn.com>
2020-05-12 15:14:43 -0700

    Add routing to yurt-fe for navigation and sharing
    E.g. /#/task/yurt-tools-frontend/frontend/yurt-fe (notice the hash)
    
    Necessarily rearchitects the data structures for easier fetching by URL.
    Also adds placeholders when waiting for the request to /detail.
    
    Future work could consist of better http error handling, and better
    handling of incomplete paths (e.g. /#/task/yurt-tools-frontend without
    the group or task)

--- internal/web/src/Sidebar.jsx
+++ internal/web/src/Sidebar.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
@@ -9,8 +11,24 @@ import green from "@material-ui/core/colors/green";
 import red from "@material-ui/core/colors/red";
 import LockIcon from "@material-ui/icons/Lock";
 import UpdateIcon from "@material-ui/icons/Update";
+import Skeleton from "@material-ui/lab/Skeleton";
 
-export default function Sidebar({ tasks, currentTask, setCurrentTask }) {
+const FakeItem = () => (
+  <ListItem button>
+    <ListItemIcon>
+      <LockIcon />
+    </ListItemIcon>
+    <ListItemIcon>
+      <UpdateIcon />
+    </ListItemIcon>
+    <ListItemText
+      primary={<Skeleton type="text" width={80} />}
+      secondary={<Skeleton type="text" width={200} />}
+    />
+  </ListItem>
+);
+
+export default function Sidebar({ jobs }) {
   const [onlySec, setOnlySec] = useState(false);
   const [onlyUpdate, setOnlyUpdate] = useState(false);
   const [searchText, setSearchText] = useState("");
@@ -40,54 +58,73 @@ export default function Sidebar({ tasks, currentTask, setCurrentTask }) {
         </ListItemText>
       </ListItem>
       <Divider />
-      {tasks
-        .filter(
-          task =>
-            task.task.includes(searchText) ||
-            task.group.includes(searchText) ||
-            task.job.includes(searchText)
-        )
-        .filter(task => !onlySec || task.vulnerable)
-        .filter(
-          task => !onlyUpdate || (task.versions && !task.versions.UpToDate)
-        )
-        .map(task => (
-          <ListItem
-            button
-            selected={currentTask && currentTask.id === task.id}
-            key={task.id}
-            onClick={() => {
-              setCurrentTask(task);
-            }}
-          >
-            <ListItemIcon>
-              {task.vulnerable != null ? (
-                task.vulnerable ? (
-                  <LockIcon style={{ color: red[200] }} />
-                ) : (
-                  <LockIcon style={{ color: green[200] }} />
-                )
-              ) : (
-                <LockIcon />
-              )}
-            </ListItemIcon>
-            <ListItemIcon>
-              {task.versions ? (
-                task.versions.UpToDate ? (
-                  <UpdateIcon style={{ color: green[200] }} />
-                ) : (
-                  <UpdateIcon style={{ color: red[200] }} />
-                )
-              ) : (
-                <UpdateIcon />
-              )}
-            </ListItemIcon>
-            <ListItemText
-              primary={`${task.task}`}
-              secondary={`${task.job} » ${task.group}`}
-            />
-          </ListItem>
-        ))}
+      {jobs
+        ? Object.entries(jobs)
+            .flatMap(([job, groups]) =>
+              Object.entries(groups).flatMap(([group, tasks]) =>
+                Object.entries(tasks).flatMap(([name, task]) => ({
+                  ...task,
+                  task: name,
+                  group: group,
+                  job: job,
+                  vulnerable: task.trivy
+                    ? task.trivy
+                        .map(target => !!target.Vulnerabilities)
+                        .reduce((acc, cur) => cur || acc, false)
+                    : null
+                }))
+              )
+            )
+            .map((task, i) => ({ ...task, id: i }))
+            .filter(
+              task =>
+                task.task.includes(searchText) ||
+                task.group.includes(searchText) ||
+                task.job.includes(searchText)
+            )
+            .filter(task => !onlySec || task.vulnerable)
+            .filter(
+              task => !onlyUpdate || (task.versions && !task.versions.UpToDate)
+            )
+            .map(task => (
+              <ListItem
+                button
+                key={task.id}
+                component={NavLink}
+                to={`/task/${task.job}/${task.group}/${task.task}`}
+                activeClassName="Mui-Selected"
+              >
+                <ListItemIcon>
+                  {task.vulnerable != null ? (
+                    task.vulnerable ? (
+                      <LockIcon style={{ color: red[200] }} />
+                    ) : (
+                      <LockIcon style={{ color: green[200] }} />
+                    )
+                  ) : (
+                    <LockIcon />
+                  )}
+                </ListItemIcon>
+                <ListItemIcon>
+                  {task.versions ? (
+                    task.versions.UpToDate ? (
+                      <UpdateIcon style={{ color: green[200] }} />
+                    ) : (
+                      <UpdateIcon style={{ color: red[200] }} />
+                    )
+                  ) : (
+                    <UpdateIcon />
+                  )}
+                </ListItemIcon>
+                <ListItemText
+                  primary={`${task.task}`}
+                  secondary={`${task.job} » ${task.group}`}
+                />
+              </ListItem>
+            ))
+        : Array(10)
+            .fill()
+            .map((_, i) => <FakeItem key={i} />)}
     </List>
   );
 }
