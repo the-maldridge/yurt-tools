634c4632a4e956c453c0fb2fa3ad9d309552ccf9
Michael Aldridge <aldridge.mac@gmail.com>
2021-11-23 14:36:39 -0600

    internal/web: Remove react frontend

--- cmd/yurt-fe/README.md
+++
@@ -1,5 +0,0 @@
-# yurt-fe
-
-The Yurt frontend serves the web interface which provides detail
-information on the data gleaned from other information sources within
-the yurt-tools suite.
--- internal/web/doc.go
+++
@@ -1,8 +0,0 @@
-// Package web contains the web-app that sits in front of the
-// registry server to provide a nice UI for end users.  This is a
-// react app which has to be built for statik inclusion.
-package web
-
-//go:generate yarn install
-//go:generate yarn run build
-//go:generate fileb0x fileb0x.yml
